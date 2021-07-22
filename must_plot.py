861d31f882f0f8d2a10ff9a2b37875809d7e9757
Emily Adams <eadams93@gwu.edu>
2021-07-22 14:36:29 -0400

    Changed labels on graph

--- must_plot.py
+++ must_plot.py
@@ -12,11 +12,11 @@ import matplotlib.pyplot as plot
 files = glob.glob('cleaned_csv_output/*.csv')
 
 
-# Takes MUST data csv files (that were sorted and cleaned by main.py) as input.
-# Calculate mean, median, standard dev, geometric mean, confidence interval (using mean and geometric mean)
+# Takes MUST data csv files (that were sorted and cleaned by main.py) as input
+# Calculates mean, median, standard dev, geometric mean, confidence interval (using mean and geometric mean)
+# Rounds to 3 decimal places
 # Function for plotting histograms with csv data
-# axis([xmin,xmax,ymin,ymax])
-# Add to csv with histogram and export
+# Calculations and histograms are then saved to same png file
 
 def gen_stats_and_histo_plot(must_data_csvs):
     png = glob.glob('cleaned_csv_output/*.png')
@@ -33,22 +33,27 @@ def gen_stats_and_histo_plot(must_data_csvs):
         std = tox_df['Value'].std().round(decimal_places)
         g_mean = stats.gmean(tox_df['Value']).round(decimal_places)
         mci = [val.round(decimal_places) for val in mean_confidence_interval(tox_df['Value'], confidence=0.95)]
-        g_mci = [val.round(decimal_places) for val in mean_confidence_interval(tox_df['Value'], confidence=0.95)]
+        g_mci = [val.round(decimal_places) for val in gmean_confidence_interval(tox_df['Value'], confidence=0.95)]
 
         x = np.array(tox_df['Value'])
 
         fig, axs = plot.subplots(1, 2, figsize=(15, 6), tight_layout=True)
         axs[0].axis('tight')
         axs[0].axis('off')
-        cell_text = [[min_val], [max_val], [mean], [median], [std], [g_mean], [mci], [g_mci]]
-        row_labels = ('Minimum Value', 'Maximum Value', 'Mean', 'Median', 'Standard Deviation', 'Geometric Mean',
-                      '95% Confidence Interval (M, –EBM, +EBM)', '95% Confidence Interval (GM, –EBGM, +EBGM)')
+        axs[0].set_title('Statistical Analysis for File')
+        cell_text = [['Statistical Analysis Values (' + (tox_df['Unit'][0]) + ')'], [tox_df.shape[0]],
+                     [tox_df['Measure'][0]], [min_val], [max_val], [mean],
+                     [median], [std], [g_mean], [mci], [g_mci]]
+        row_labels = ('Value/Calculation', 'Number of Threshold Measurements', 'Threshold Measurement Type',
+                      'Minimum Value', 'Maximum Value', 'Mean', 'Median', 'Standard Deviation',
+                      'Geometric Mean', '95% Confidence Interval (M, –EBM, +EBM)',
+                      '95% Confidence Interval (GM, –EBGM, +EBGM)')
         axs[0].table(cellText=cell_text, rowLabels=row_labels, colLabels=None, colWidths=[1, 1], loc='center',
-                     fontsize=25)
-        axs[1].hist(x, bins=5)
+                     fontsize=20)
+        axs[1].hist(x, edgecolor='k', bins=5)
         axs[1].axis([0, x.max() + 1, 0, len(x)])
         axs[1].set_ylabel('Frequency')
-        axs[1].set_xlabel('Threshold Values')
+        axs[1].set_xlabel('Category of Concern ' + ' (' + tox_df['Unit'][0] + ')')
         axs[1].set_title('Plot for File: ' + str(f))
         fig.savefig('plots_and_stats_output/{}_{}_plot_and_stats.png'.format(tox_df['CAS'][0], tox_df['Measure'][0]))
         plot.close(fig)
